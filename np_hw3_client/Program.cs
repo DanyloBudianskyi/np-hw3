82cfad17ff5ffe8b2e50b960cc39f19f7917429a
Danil <super.danil501@gmail.com>
2024-12-19 23:09:33 -0700

    Add project files.

---
+++ np_hw3_client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+        Console.WriteLine("Use TURN_ON or TURN_OFF");
+        Thread writeThread = new Thread(() => WriteToServer(stream));
+        Thread listenThread = new Thread(() => ListenServer(stream));
+        writeThread.Start();
+        listenThread.Start();
+
+    }
+    static void WriteToServer(NetworkStream stream)
+    {
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+    static void ListenServer(NetworkStream stream)
+    {
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Server: {response}");
+        }
+    }
+}
---
+++ np_hw3_server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace np_hw3_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server has started and working...");
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("User connected...");
+                Thread thread = new Thread(HandleClient);
+                thread.Start(client);
+
+            }
+            static void HandleClient(object obj)
+            {
+                TcpClient client = (TcpClient)obj;
+                NetworkStream stream = client.GetStream();
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine($"User: {message}");
+                        if (message.ToUpper() == "TURN_ON" || message.ToUpper() == "TURN ON")
+                        {
+                            buffer = Encoding.UTF8.GetBytes("LIGTH ON");
+                            stream.Write(buffer, 0, buffer.Length);
+                        }
+                        else if (message.ToUpper() == "TURN_OFF" || message.ToUpper() == "TURN OFF")
+                        {
+                            buffer = Encoding.UTF8.GetBytes("LIGTH OFF");
+                            stream.Write(buffer, 0, buffer.Length);
+                        }
+                        else
+                        {
+                            buffer = Encoding.UTF8.GetBytes("UNKNOWM COMMAND");
+                            stream.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+
+                
+            }
+        }
+
+    }
+}
